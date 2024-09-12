e25f15322f1a43ff9021752f581cb612dbc09141
Mohammedjebali <jebalimohammed7@gmail.com>
2024-09-12 17:19:25 +0100

    reactcheckpoint1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react'
 
-export default App;
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      fullname:"mohamed",
+      bio: "elelelelele",
+      img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Fvector-art%2F14554760-man-profile-negative-photo-anonymous-silhouette-human-head-businessman-worker-support-vector-illustration&psig=AOvVaw17oKY6r_eZntaVCqiyFXNo&ust=1726244305360000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMjLmKfnvYgDFQAAAAAdAAAAABAE",
+      profession: "student",
+      show: false
+    }
+  }
+   
+  HideProfile=()=> {
+    this.setState({show:!this.state.show})
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.HideProfile}>{this.state.show==true?"hide":"show"} profile </button>
+
+        {this.state.show==true? <div>
+        <h1>{this.state.fullname}</h1>
+        <h4>{this.state.bio}</h4>
+        <img src={this.state.img} />
+        <p>{this.state.profession}</p>
+        </div>:null}
+      </div>
+    )
+  }
+}
